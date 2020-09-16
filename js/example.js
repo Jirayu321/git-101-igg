7a475232ee1253002201d2be43c7346f6e35c9e7
Jirayu321 <50855666+Jirayu321@users.noreply.github.com>
2020-09-16 13:37:58 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,22 @@
+
+// // const PI = 3.14 // const ค่าที่จะไม่เปลี่ยนมัน
+// // // const name = "OOE"
+// let name = "o"
+//     name ='A'
+//     name ="I"//มันจะrunแค่บรรทัดสุดท้าย
+// let test ="Hello"
+//  name =`${test} B`    
+// console.log("name:",name)
+
+let arr = ['apple','banana','kiwi' ]
+// console.log("arr:",arr[1])
+
+let  person = {
+    name: 'Ooe',
+    age: '20',
+    gender: {
+        name: 'male'
+    },
+    arr
+}
+console.log(person)
