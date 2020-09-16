1e228560464d58f288e7e940daa79106324d6812
Jirayu321 <50855666+Jirayu321@users.noreply.github.com>
2020-09-16 15:25:37 +0700

    Update quiz3.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -6,7 +6,13 @@ const checkgrade = (socre) =>{
         grade="B"
     }else if(socre >= 61 && socre <=70){
         grade="C"
+    }else if(socre >= 51 && socre <=60){
+        grade="D"
+    }else if(socre >= 0 && socre <=50){
+        grade="F"
+    }else {
+        grade="socre was invalid"
     }
-
+    return grade
 }
-
+console.log(checkgrade(1))
